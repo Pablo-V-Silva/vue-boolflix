4e9639b04f320306e04c3d2be67e25c93e854f70
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-18 11:30:27 +0100

    add the searchbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <SiteHeader />
+    <SiteMain />
+    <SiteFooter />
+  </div>
 </template>
 
 <script>
+import SiteHeader from "./components/site_header.vue";
+import SiteMain from "./components/site_main.vue";
+import SiteFooter from "./components/site_footer.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    SiteHeader,
+    SiteMain,
+    SiteFooter,
+  },
 };
 </script>
 
---
+++ src/components/site_header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <form>
+      <input type="text" placeholder="Search a Film" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
